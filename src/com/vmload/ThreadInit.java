39a29891f8155db4e2b06a03fee12bf92a87299b
dy <2711099209@qq.com>
2016-08-02 18:26:27 +0800

    多线程初始化类，类只会被初始化一次。

---
+++ src/com/vmload/ThreadInit.java
@@ -0,0 +1,27 @@
+package com.vmload;
+
+class TestThread {
+	static {
+		System.out.println(Thread.currentThread() + "我被初始化！");
+	}
+}
+
+public class ThreadInit {
+
+	public static void main(String[] args) {
+		Runnable s = new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println(Thread.currentThread() + "线程开始");
+				TestThread t = new TestThread();
+				System.out.println(Thread.currentThread() + "线程结束");
+			}
+		};
+		Thread t1 = new Thread(s);
+		Thread t2 = new Thread(s);
+		t1.start();
+		t2.start();
+	}
+
+}
