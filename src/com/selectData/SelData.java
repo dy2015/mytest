299b29ac2f6bc2bdbdbadbf0ad5f700858885aed
dy <2711099209@qq.com>
2016-09-09 17:54:24 +0800

    新增测试

--- src/com/selectData/SelData.java
+++ src/com/selectData/SelData.java
@@ -2,6 +2,8 @@ package com.selectData;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class SelData {
 	public static void compute(List<Tag> tagList, List<Tag> tagListTmp, String curPageTagId) {
@@ -51,6 +53,64 @@ public class SelData {
 			for (Tag t : tagListTmp) {
 				System.out.print(t.getId() + ",");
 			}
+		}
+		
+		Map<Integer, List<Integer>> seqIdeaIdMap = new TreeMap<Integer, List<Integer>>();
+		
+		for (int i=0;i<10;i++) {
+			List<Integer> ideaIds = new ArrayList<Integer>();
+			ideaIds.add(i);
+			if (seqIdeaIdMap.get(1) != null) {
+				seqIdeaIdMap.get(1).addAll(ideaIds);
+			} else {
+				seqIdeaIdMap.put(1, ideaIds);
+			}
+		}
+		for (Integer seq : seqIdeaIdMap.keySet()) {
+			System.out.println("key="+seq);
+			for (Integer ideaId : seqIdeaIdMap.get(seq)) {
+				System.out.println("value="+ideaId);
+			}
+		}
+		
+		List<Integer> testList=new ArrayList<>();
+		testList.add(1);
+		testList.add(2);
+		testList.add(3);
+		testList.add(4);
+		testList.remove(testList.size()-1);
+		for(Integer i:testList){
+			System.out.println(i);
+		}
+		String listStr=testList.toString();
+		System.out.println("listStr="+listStr);
+		
+		Student s=new Student();
+		s.setName(123);
+		int tempStr=s.getName();
+		System.out.println("tempStr="+tempStr);
+		tempStr=tempStr+7;
+		System.out.println("s.getName()="+s.getName());
+		
+		Map<String,String> map=null;
+		for (String cookieName : map.keySet()) {
+            String cookieValue = map.get(cookieName);
+            int j = cookieValue.indexOf("_");
+            if (j <= 0) {
+                continue;
+            }
+		}
+		
+	}
+	static class Student{
+		private int name ;
+
+		public int getName() {
+			return name;
+		}
+
+		public void setName(int name) {
+			this.name = name;
 		}
 	}
 }
